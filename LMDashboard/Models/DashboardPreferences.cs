3b7280335084c10fbb37fb6aef2589c28cfa7328
jmwny <jmwny@users.noreply.github.com>
2026-03-02 17:48:11 -0500

    Add glitch and matrix visual effects with user toggles... because, why not.

--- LMDashboard/Models/DashboardPreferences.cs
+++ LMDashboard/Models/DashboardPreferences.cs
@@ -7,5 +7,7 @@ public class DashboardPreferences
     public bool Scanlines { get; set; } = true;
     public bool Vignette { get; set; } = true;
     public bool ShowFailingOnly { get; set; }
+    public bool Glitch { get; set; } = true;
+    public bool Matrix { get; set; } = true;
     public double Brightness { get; set; } = 1.0;
 }
